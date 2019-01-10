d5cf5acc23dd60c5edf39b8a60c8b7b7c886004e
chencheng <sorrycc@gmail.com>
2019-01-10 17:10:46 +0800

    initial commit

---
+++ cli.js
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+
+const { existsSync, readdirSync } = require('fs');
+const { join } = require('path');
+const yParser = require('yargs-parser');
+const chalk = require('chalk');
+const inquirer = require('inquirer');
+const clipboardy = require('clipboardy');
+const emptyDir = require('empty-dir');
+
+// print version and @local
+const args = yParser(process.argv.slice(2));
+if (args.v || args.version) {
+  console.log(require('../package').version);
+  if (existsSync(join(__dirname, '../.local'))) {
+    console.log(chalk.cyan('@local'));
+  }
+  process.exit(0);
+}
+
+// make sure it's empty
+if (emptyDir.sync(__dirname)) {
+  console.error(chalk.red(`> Current dir is not empty`));
+  process.exit(1);
+}
+
+const generators = readdirSync(`${__dirname}/lib/generators`)
+  .filter(f => !f.startsWith('.'));
+inquirer.prompt([
+  {
+    name: 'type',
+    message: 'Select the boilerplate type',
+    type: 'list',
+    choices: generators,
+  },
+]).then(answers => {
+  runGenerator(`./lib/generators/${answers.type}`);
+}).catch(e => {
+  console.error(chalk.red(`> Generate failed`));
+  console.log(e);
+  process.exit(1);
+});
+
+function runGenerator(generatorPath) {
+  const Generator = require(generatorPath);
+  const generator = new Generator(process.argv.slice(2), {
+    name: '_',
+    env: {
+      cwd: process.cwd(),
+    },
+    resolved: require.resolve(generatorPath),
+  });
+  generator.run(() => {
+    if (args._[0]) {
+      clipboardy.writeSync(`cd ${args._[0]}`);
+      console.log('📋  Copied to clipboard, just use Ctrl+V');
+    }
+    console.log('✨  File Generate Done');
+  });
+}
---
+++ lib/generators/node/templates/README.md
@@ -0,0 +1,43 @@
+# <%= name %>
+
+Description.
+
+[![NPM version][npm-image]][npm-url]
+[![build status][travis-image]][travis-url]
+[![Test coverage][codecov-image]][codecov-url]
+[![David deps][david-image]][david-url]
+[![Known Vulnerabilities][snyk-image]][snyk-url]
+[![npm download][download-image]][download-url]
+
+[npm-image]: https://img.shields.io/npm/v/<%= name %>.svg?style=flat-square
+[npm-url]: https://npmjs.org/package/<%= name %>
+[travis-image]: https://img.shields.io/travis/<%= repo %>/<%= name %>.svg?style=flat-square
+[travis-url]: https://travis-ci.org/<%= repo %>/<%= name %>
+[codecov-image]: https://codecov.io/gh/<%= repo %>/<%= name %>/branch/master/graph/badge.svg
+[codecov-url]: https://codecov.io/gh/<%= repo %>/<%= name %>
+[david-image]: https://img.shields.io/david/<%= repo %>/<%= name %>.svg?style=flat-square
+[david-url]: https://david-dm.org/<%= repo %>/<%= name %>
+[snyk-image]: https://snyk.io/test/npm/<%= name %>/badge.svg?style=flat-square
+[snyk-url]: https://snyk.io/test/npm/<%= name %>
+[download-image]: https://img.shields.io/npm/dm/<%= name %>.svg?style=flat-square
+[download-url]: https://npmjs.org/package/<%= name %>
+
+## Why
+
+## Feature
+
+* ✔︎ A
+* ✔︎ B
+* ✘︎ C
+
+## Installation
+
+```bash
+$ yarn global add <%= name %>
+```
+
+## Usage
+
+## LICENSE
+
+MIT
